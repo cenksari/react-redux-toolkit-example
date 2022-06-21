bcc86ffe810c2a0c076fdf06cada99c4740c42df
Cenk SARI <cenk@cenksari.com>
2022-06-21 16:39:45 +0300

    packages updated

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
 import ProfilePage from '../pages/ProfilePage';
